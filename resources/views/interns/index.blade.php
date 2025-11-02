107b0acc866022783abcf51622f0ef71f8fbca48
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-02 15:06:54 -0300

    :sparkles: feat: adiciona link para rota interns.create

--- resources/views/interns/index.blade.php
+++ resources/views/interns/index.blade.php
@@ -1,5 +1,5 @@
 <h1>Listagem de estagiários</h1>
-<a href="">+ Adicionar</a>
+<a href="{{ route('interns.create') }}">+ Adicionar</a>
 @foreach ($interns as $intern)
     <p>{{ $intern->name }} | <a href="">✏</a> | <form action="" method="post"></form> </p>
 @endforeach
