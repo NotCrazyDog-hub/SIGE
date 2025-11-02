e76b7b9654f2a3ff75e0aaac4874b42bbb53a3eb
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-02 15:43:59 -0300

    :bug: fix: substitui placeholder por value nos inputs do form edit

--- resources/views/interns/edit.blade.php
+++ resources/views/interns/edit.blade.php
@@ -2,13 +2,13 @@
 <form action="{{ route('interns.edit', ['intern' => $intern->id]) }}" method="post">
     @csrf
     @method('PUT')
-    <input type="text" name="name" placeholder="{{ $intern->name }}">
+    <input type="text" name="name" value="{{ $intern->name }}">
     <br>
-    <input type="email" name="email" placeholder="{{ $intern->email }}">
+    <input type="email" name="email" value="{{ $intern->email }}">
     <br>
-    <input type="text" name="course" placeholder="{{ $intern->course }}">
+    <input type="text" name="course" value="{{ $intern->course }}">
     <br>
-    <input type="text" name="phone" placeholder="{{ $intern->phone }}">
+    <input type="text" name="phone" value="{{ $intern->phone }}">
     <br>
     <input type="hidden" name="status" value="0">
     <input type="checkbox" name="status" value="1" {{ $intern->status ? 'checked' : '' }}>
