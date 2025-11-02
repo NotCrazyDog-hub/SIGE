ac719618d0430e16691742ffae01103e6f66b9ba
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-02 15:24:55 -0300

    :sparkles: feat: adiciona link com parâmetro para rota interns.edit

--- resources/views/interns/index.blade.php
+++ resources/views/interns/index.blade.php
@@ -1,5 +1,5 @@
 <h1>Listagem de estagiários</h1>
 <a href="{{ route('interns.create') }}">+ Adicionar</a>
 @foreach ($interns as $intern)
-    <p>{{ $intern->name }} | <a href="">✏</a> | <form action="" method="post"></form> </p>
+    <p>{{ $intern->name }} | <a href="{{ route('interns.edit', ['intern' => $intern->id]) }}">✏</a> | <form action="" method="post"></form> </p>
 @endforeach
