3bf75eb51af27c7d7c7ce5f719d35178344da2b3
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-02 15:15:01 -0300

    :sparkles: feat: cria estrutura básica da view create.blade.php

---
+++ resources/views/interns/create.blade.php
@@ -0,0 +1,16 @@
+<h1>Cadastro de estagiários</h1>
+<form action="{{ route('interns.store') }}" method="get">
+    @csrf
+    <input type="text" name="name" placeholder="Insira seu nome">
+    <br>
+    <input type="email" name="email" placeholder="Insira seu email">
+    <br>
+    <input type="text" name="course" placeholder="Insira seu curso">
+    <br>
+    <input type="text" name="phone" placeholder="Insira seu telefone">
+    <br>
+    <input type="hidden" name="status" value="0">
+    <input type="checkbox" name="status" value="1">
+    <br>
+    <input type="submit" value="Cadastrar">
+</form>
