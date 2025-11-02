b154bf1542934d4b6aa9b5f729a1245ffb0c449e
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-02 15:59:35 -0300

    :sparkles: feat: implementa formulário completo de delete

--- resources/views/interns/index.blade.php
+++ resources/views/interns/index.blade.php
@@ -1,5 +1,8 @@
 <h1>Listagem de estagiários</h1>
 <a href="{{ route('interns.create') }}">+ Adicionar</a>
 @foreach ($interns as $intern)
-    <p>{{ $intern->name }} | <a href="{{ route('interns.edit', ['intern' => $intern->id]) }}">✏</a> | <form action="" method="post"></form> </p>
+    <p>{{ $intern->name }} | <a href="{{ route('interns.edit', ['intern' => $intern->id]) }}">✏</a> | <form action="{{ route('interns.destroy', ['intern' => $intern->id]) }}" method="post">
+        @method('DELETE') 
+        <input type="submit" onclick="return confirm('Tem certeza que deletar este estagiário?')" value="🗑">
+    </form></p>
 @endforeach
