8fe5293c54dc43c6e3bfa9d156a08b235c2360f5
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-02 14:33:27 -0300

    :wrench: chore: preenche migration de estagiários

--- database/migrations/2025_11_02_172731_create_interns_table.php
+++ database/migrations/2025_11_02_172731_create_interns_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('interns', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('course');
+            $table->string('phone');
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
