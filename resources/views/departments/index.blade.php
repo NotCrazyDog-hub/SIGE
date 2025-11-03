6ce650d59a11d1b737c52d1f8b4fe44870d596cb
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-03 20:34:21 -0300

    :sparkles: feat: cria link de edição com parâmetro na listagem de departamentos

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -1,5 +1,5 @@
 <h1>Listagem de departamentos</h1>
 <a href="{{ route('departments.create') }}">+ Adicionar</a>
 @foreach ($departments as $department)
-    <p>{{ $department->name }} | <a href="">✏</a> | <form action="" method=""></form> </p>
+    <p>{{ $department->name }} | <a href="{{ route('departments.edit', ['department' => $department->id]) }}">✏</a> | <form action="" method=""></form> </p>
 @endforeach
