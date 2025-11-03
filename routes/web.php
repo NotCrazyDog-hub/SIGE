c1e47e33fdb061b8dca0bbbb5780e6395eb371e5
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2025-11-03 16:39:17 -0300

    :wrench: chore: importa DepartmentController no arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InternController;
+use App\Http\Controllers\DepartmentController;
 
 Route::get('/', function () {
     return view('welcome');
