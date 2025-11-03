28aecb8d848ba36e6a5a36d4106067cd4f32ffdb
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-02 21:14:13 -0300

    :sparkles: chore: define restrição de unicidade ao campo email na tabela interns

--- database/migrations/2025_11_02_172731_create_interns_table.php
+++ database/migrations/2025_11_02_172731_create_interns_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('interns', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('course');
             $table->string('phone');
             $table->boolean('status')->default(true);
