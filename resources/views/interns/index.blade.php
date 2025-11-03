851ac135e847e40e749dfe03d6a8a7ba4bd8ccf1
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2025-11-03 16:55:27 -0300

    :bug: fix: corrige erro de página expirada ao excluir estagiário

--- resources/views/interns/index.blade.php
+++ resources/views/interns/index.blade.php
@@ -2,6 +2,7 @@
 <a href="{{ route('interns.create') }}">+ Adicionar</a>
 @foreach ($interns as $intern)
     <p>{{ $intern->name }} | <a href="{{ route('interns.edit', ['intern' => $intern->id]) }}">✏</a> | <form action="{{ route('interns.destroy', ['intern' => $intern->id]) }}" method="post">
+        @csrf
         @method('DELETE') 
         <input type="submit" onclick="return confirm('Tem certeza que deletar este estagiário?')" value="🗑">
     </form></p>
