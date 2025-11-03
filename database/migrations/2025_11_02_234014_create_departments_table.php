8c3e32164a0f72f063f259956ad474af331feb1a
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-02 21:09:45 -0300

    :wrench: chore: adiciona colunas name, location e manager na tabela departments

--- database/migrations/2025_11_02_234014_create_departments_table.php
+++ database/migrations/2025_11_02_234014_create_departments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('location');
+            $table->string('manager');
             $table->timestamps();
         });
     }
