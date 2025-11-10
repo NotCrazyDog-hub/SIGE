e0379034171e5d7c8cd671406f274d7554c9ac99
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2025-11-10 10:47:52 -0300

    :bug: fix: adiciona parâmetro para form delete

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -1,7 +1,7 @@
 <h1>Listagem de departamentos</h1>
 <a href="{{ route('departments.create') }}">+ Adicionar</a>
 @foreach ($departments as $department)
-    <p>{{ $department->name }} | <a href="{{ route('departments.edit', ['department' => $department->id]) }}">✏</a> | <form action="{{ route('departments.destroy') }}" method="post">
+    <p>{{ $department->name }} | <a href="{{ route('departments.edit', ['department' => $department->id]) }}">✏</a> | <form action="{{ route('departments.destroy', ['department' => $department->id]) }}" method="post">
         @csrf
         @method('DELETE')
         <input type="submit" onclick="return confirm('Tem certeza que deseja excluir esse departamento?')" value="🗑">
