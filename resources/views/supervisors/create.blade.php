ab5f0cea44209de156bcb5e8e297eaa83ce2db21
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-11 20:16:50 -0300

    :sparkles: feat: cria estrutura básica da view create.blade.php na pasta supervisors

---
+++ resources/views/supervisors/create.blade.php
@@ -0,0 +1,17 @@
+<h1>Cadastro de supervisores</h1>
+<form action="{{ route('supervisors.store') }}" method="post">
+    @csrf
+    <input type="text" name="name" placeholder="Insira o nome do supervisor">
+    <br>
+    <select name="department_id">
+        @foreach ($departments as $department)
+            <option value="{{ $department->id }}">{{ $department->name }}</option>
+        @endforeach
+    </select>
+    <br>
+    <input type="email" name="email" placeholder="Insira o email do supervisor">
+    <br>
+    <input type="text" name="phone" placeholder="Insira o telefone do supervisor">
+    <br>
+    <input type="submit" value="Cadastrar">
+</form>
