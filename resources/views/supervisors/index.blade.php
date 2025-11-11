7cb225395b2996637da28c3de3569f3810906a7d
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-11 20:46:42 -0300

    :sparkles: feat: adiciona linkagem com a rota supervisors.edit

--- resources/views/supervisors/index.blade.php
+++ resources/views/supervisors/index.blade.php
@@ -1,7 +1,7 @@
 <h1>Listagem de supervisores</h1>
 <a href="{{ route('supervisors.create') }}">+ Adicionar</a>
 @foreach ($supervisors as $supervisor)
-    <p> | <a href="">✏</a> | <form action="" method="post">
+    <p> | <a href="{{ route('supervisors.edit', ['supervisor' => $supervisor->id]) }}">✏</a> | <form action="" method="post">
         @csrf
         @method('DELETE') 
         <input type="" onclick="return confirm('Tem certeza que deletar este supervisor?')" value="🗑">
