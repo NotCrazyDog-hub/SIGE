fa2d67d9a11057fbd3e55538f2e8f0e12d0058dc
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-11 21:13:35 -0300

    :sparkles: feat: cria estrutura básica da view edit.blade.php na pasta supervisors

---
+++ resources/views/supervisors/edit.blade.php
@@ -0,0 +1,19 @@
+<h1>Edição de supervisores</h1>
+<form action="{{ route('supervisors.update', ['supervisor' => $supervisor->id]) }}" method="post">
+    @csrf
+    @method('PUT')
+    <input type="text" name="name" value="{{ $supervisor->name }}">
+    <br>
+    <select name="department_id">
+        <option disabled selected>Selecione um departamento</option>
+        @foreach ($departments as $department)
+            <option value="{{ $department->id }}" {{ $departmeny->id == $supervisor->department_id ? 'checked' : '' }}>{{ $department->name }}</option>
+        @endforeach
+    </select>
+    <br>
+    <input type="email" name="email" value="{{ $supervisor->email }}">
+    <br>
+    <input type="text" name="phone" value="{{ $supervisor->phone }}">
+    <br>
+    <input type="submit" value="Atualizar">
+</form>
