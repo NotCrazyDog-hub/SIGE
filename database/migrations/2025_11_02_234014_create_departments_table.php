61c665ccefaf73a7b53500069fc0351015e325fa
NotCrayDog-hub <pedro.olivera.ncd@gmail.com>
2025-11-13 12:38:56 -0300

    :fire: chore: remove campos 'location' e 'manager' da tabela 'departments'

--- database/migrations/2025_11_02_234014_create_departments_table.php
+++ database/migrations/2025_11_02_234014_create_departments_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('location');
-            $table->string('manager');
             $table->timestamps();
         });
     }
