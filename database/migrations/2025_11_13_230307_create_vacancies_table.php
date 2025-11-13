b857bbd48b37bc9af580f82e98f9520dd780b5d6
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-13 20:13:40 -0300

    :wrench: chore: adiciona colunas department_id, title, description e workload na tabela vacancies

--- database/migrations/2025_11_13_230307_create_vacancies_table.php
+++ database/migrations/2025_11_13_230307_create_vacancies_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('vacancies', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('department_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('workload');
             $table->timestamps();
         });
     }
