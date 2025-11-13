8f9f7390b5d190433a321ba90895360ec1b0560e
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-13 20:32:21 -0300

    :wrench: chore: importa VacancyController no arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InternController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\SupervisorController;
+use App\Http\Controllers\VacancyController;
 
 Route::get('/', function () {
     return view('welcome');
