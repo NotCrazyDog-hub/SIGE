153cf4ca92e353c6d2b254b130a26ab6a3d46b87
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-15 08:29:27 -0300

    :sparkles: feat: adiciona estrutura básica para index.blade.php de vagas

---
+++ resources/views/vacancies/index.blade.php
@@ -0,0 +1,9 @@
+<h1>Listagem de vagas</h1>
+<a href="{{ route('vacancies.create') }}">+ Adicionar</a>
+@foreach ($vacancies as $vacancy)
+    <p>{{ $vacancy->name }} | <a href="{{ route('vacancies.edit', ['vacancy' => $vacancy->id]) }}">✏</a> | <form action="{{ route('vacancies.destroy', ['vacancy' => $vacancy->id]) }}" method="post">
+        @csrf
+        @method('DELETE') 
+        <input type="submit" onclick="return confirm('Tem certeza que deletar esta vaga?')" value="🗑">
+    </form></p>
+@endforeach
