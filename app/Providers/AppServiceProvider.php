3e008db760842bdce05a08717b1189fadc4d3698
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-17 21:13:33 -0300

    :wrench: chore: configura Carbon para exibir mensagens em português

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+
+        Carbon::setLocale('pt_BR');
     }
 }
