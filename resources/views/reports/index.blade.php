be75fe954a8f4197be86c5fc413d8fcd18b44908
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-19 20:48:51 -0300

    :sparkles: feat: adiciona estrutura básica para index.blade.php de relatórios

---
+++ resources/views/reports/index.blade.php
@@ -0,0 +1,9 @@
+<h1>Listagem de relatórios</h1>
+<a href="{{ route('reports.create') }}">+ Adicionar</a>
+@foreach ($reports as $report)
+    <p> {{ $report->title}} | <a href="{{ route('reports.edit', ['report' => $report->id]) }}">✏</a> | <form action="{{ route('reports.destroy', ['report' => $report->id]) }}" method="post">
+        @csrf
+        @method('DELETE') 
+        <input type="submit" onclick="return confirm('Tem certeza que deletar este relatório?')" value="🗑">
+    </form></p>
+@endforeach
