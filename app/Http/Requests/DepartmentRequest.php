157f8ee44e25cb0fbdb70c3c439bfe63ab0e2e77
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-20 12:32:40 -0300

    :sparkles: feat: adiciona as regras dos campos no DepartmentRequest

--- app/Http/Requests/DepartmentRequest.php
+++ app/Http/Requests/DepartmentRequest.php
@@ -22,7 +22,8 @@ class DepartmentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:500',
         ];
     }
 }
