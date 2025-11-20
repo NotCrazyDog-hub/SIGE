dc827f6d557d38d147336a899be5144a2c96f526
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-20 12:54:20 -0300

    :recycle: refactor: usa SupervisorRequest nos métodos store e update

--- app/Http/Controllers/SupervisorController.php
+++ app/Http/Controllers/SupervisorController.php
@@ -29,7 +29,7 @@ class SupervisorController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(SupervisorRequest $request)
     {
         $supervisor = Supervisor::create($request->all());
         return redirect()->route('supervisors.index');
@@ -56,7 +56,7 @@ class SupervisorController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(SupervisorRequest $request, string $id)
     {
         $supervisor = Supervisor::findOrFail($id);
         $supervisor->update($request->except(['_token', '_method']));
