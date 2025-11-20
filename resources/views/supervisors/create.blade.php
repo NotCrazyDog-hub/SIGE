d2f14ddbaa1bb7906a5db6761af1941cedf4c519
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-20 13:13:14 -0300

    :sparkles: feat: adiciona mensagens de erro para cada campo no form create e edit de supervisores

--- resources/views/supervisors/create.blade.php
+++ resources/views/supervisors/create.blade.php
@@ -1,8 +1,14 @@
 <h1>Cadastro de supervisores</h1>
 <form action="{{ route('supervisors.store') }}" method="post">
     @csrf
+    @error('name')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="text" name="name" placeholder="Insira o nome do supervisor">
     <br>
+    @error('department_id')
+        <p>{{ $message }}</p>
+    @enderror
     <select name="department_id">
         <option disabled selected>Selecione um departamento</option>
         @foreach ($departments as $department)
@@ -10,8 +16,14 @@
         @endforeach
     </select>
     <br>
+    @error('email')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="email" name="email" placeholder="Insira o email do supervisor">
     <br>
+    @error('phone')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="text" name="phone" placeholder="Insira o telefone do supervisor">
     <br>
     <input type="submit" value="Cadastrar">
--- resources/views/supervisors/edit.blade.php
+++ resources/views/supervisors/edit.blade.php
@@ -2,16 +2,28 @@
 <form action="{{ route('supervisors.update', ['supervisor' => $supervisor->id]) }}" method="post">
     @csrf
     @method('PUT')
+    @error('name')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="text" name="name" value="{{ $supervisor->name }}">
     <br>
+    @error('department_id')
+        <p>{{ $message }}</p>
+    @enderror
     <select name="department_id">
         @foreach ($departments as $department)
             <option value="{{ $department->id }}" {{ $department->id == $supervisor->department_id ? 'selected' : '' }}>{{ $department->name }}</option>
         @endforeach
     </select>
     <br>
+    @error('email')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="email" name="email" value="{{ $supervisor->email }}">
     <br>
+    @error('phone')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="text" name="phone" value="{{ $supervisor->phone }}">
     <br>
     <input type="submit" value="Atualizar">
