ce9c0164f0d541399227de8a37a5b188f8d0b505
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-20 20:03:21 -0300

    :sparkles: feat: adiciona mensagens de erro para cada campo no form create e edit de relatórios

--- resources/views/reports/create.blade.php
+++ resources/views/reports/create.blade.php
@@ -1,8 +1,14 @@
 <h1>Registro de relatórios</h1>
 <form action="{{ route('reports.store') }}" method="post">
     @csrf
+     @error('title')
+        <p>{{ $message }}</p>       
+    @enderror 
     <input type="text" name="title" placeholder="Insira o título do relatório">
     <br>
+    @error('intern_id')
+        <p>{{ $message }}</p>       
+    @enderror 
     <select name="intern_id">
         <option disabled selected>Selecione um estagiário</option>
         @foreach ($interns as $intern)
@@ -10,8 +16,14 @@
         @endforeach
     </select>
     <br>
+    @error('work_description')
+        <p>{{ $message }}</p>       
+    @enderror 
     <textarea name="work_description" placeholder="Insira a descrição das atividades"></textarea>
     <br>
+    @error('grade')
+        <p>{{ $message }}</p>       
+    @enderror  
     <input type="number" name="grade" placeholder="Insira a nota das atividades">
     <br>
     <input type="submit" value="Registrar">
--- resources/views/reports/edit.blade.php
+++ resources/views/reports/edit.blade.php
@@ -2,16 +2,28 @@
 <form action="{{ route('reports.update', ['report' => $report->id]) }}" method="post">
     @csrf
     @method('PUT')
+    @error('title')
+        <p>{{ $message }}</p>  
+    @enderror
     <input type="text" name="title" value="{{ $report->title }}">
     <br>
+    @error('intern_id')
+        <p>{{ $message }}</p>
+    @enderror
     <select name="intern_id">
         @foreach ($interns as $intern)
             <option value="{{ $intern->id }}" {{ $intern->id == $report->intern_id ? 'selected' : '' }}>{{ $intern->name }}</option>
         @endforeach
     </select>
     <br>
+    @error('work_description')
+        <p>{{ $message }}</p>
+    @enderror
     <textarea name="work_description">{{ $report->work_description }}</textarea>
     <br>
+    @error('grade')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="number" name="grade" value="{{ $report->grade }}">
     <br>
     <input type="submit" value="Atualizar">
