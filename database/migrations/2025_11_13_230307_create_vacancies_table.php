861b8ef0145ae4ccd9a24f614d1a5f74520da59a
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-20 14:18:28 -0300

    refactor: ajusta coluna 'workload' para string ao invés de integer

--- database/migrations/2025_11_13_230307_create_vacancies_table.php
+++ database/migrations/2025_11_13_230307_create_vacancies_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('department_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('description');
-            $table->integer('workload');
+            $table->string('workload');
             $table->timestamps();
         });
     }
