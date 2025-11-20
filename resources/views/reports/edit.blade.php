fcac68ff1b7d71e6086ee3171d4b9d2c5f3f5df2
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-19 21:23:31 -0300

    :sparkles: feat: adiciona estrutura básica para edit.blade.php de relatórios

---
+++ resources/views/reports/edit.blade.php
@@ -0,0 +1,17 @@
+<h1>Edição de relatórios</h1>
+<form action="{{ route('reports.update', ['report' => $report->id]) }}" method="post">
+    @csrf
+    <input type="text" name="title" value="{{ $report->title }}">
+    <br>
+    <select name="intern_id">
+        @foreach ($interns as $intern)
+            <option value="{{ $intern->id }}" {{ $intern->id == $report->intern_id ? 'selected' : '' }}>{{ $intern->name }}</option>
+        @endforeach
+    </select>
+    <br>
+    <textarea name="work_description">{{ $report->work_description }}</textarea>
+    <br>
+    <input type="number" name="grade" value="{{ $report->grade }}">
+    <br>
+    <input type="submit" value="Atualizar">
+</form>
