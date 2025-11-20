a9fc8621cfb1552f363468e0315178bdbf9d044c
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-20 14:28:38 -0300

    :sparkles: feat: adiciona mensagens de erro para cada campo no form create e edit de vagas de estágios

--- resources/views/vacancies/edit.blade.php
+++ resources/views/vacancies/edit.blade.php
@@ -2,17 +2,29 @@
 <form action="{{ route('vacancies.update', ['vacancy' => $vacancy->id]) }}" method="post">
     @csrf
     @method('PUT')
+    @error('title')
+        <<p>{{ $message }}</p>
+    @enderror
     <input type="text" name="title" value="{{ $vacancy->title }}">
     <br>
+    @error('department_id')
+        <<p>{{ $message }}</p>
+    @enderror
     <select name="department_id">
         <option disabled selected>Selecione um departamento</option>
         @foreach ($departments as $department)
             <option value="{{ $department->id }}" {{ $department->id == $vacancy->department_id ? 'selected' : '' }}>{{ $department->name }}</option>
         @endforeach
     </select>
+    @error('description')
+        <<p>{{ $message }}</p>
+    @enderror
     <br>
     <textarea name="description">{{ $vacancy->description}}</textarea>
     <br>
+    @error('workload')
+        <<p>{{ $message }}</p>
+    @enderror
     <input type="text" name="workload" value="{{ $vacancy->workload }}">
     <br>
     <input type="submit" value="Atualizar">
