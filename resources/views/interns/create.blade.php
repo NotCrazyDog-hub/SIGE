e2f1a042ff2800f8b419c9b8684e0c538a492abd
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-20 12:33:52 -0300

    :sparkles: feat: adiciona mensagens de erro para cada campo no form create de estagiários

--- resources/views/interns/create.blade.php
+++ resources/views/interns/create.blade.php
@@ -1,12 +1,24 @@
 <h1>Cadastro de estagiários</h1>
 <form action="{{ route('interns.store') }}" method="post">
     @csrf
+    @error('name')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="text" name="name" placeholder="Insira seu nome">
     <br>
+    @error('email')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="email" name="email" placeholder="Insira seu email">
     <br>
+    @error('course')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="text" name="course" placeholder="Insira seu curso">
     <br>
+    @error('phone')
+        <p>{{ $message }}</p>
+    @enderror
     <input type="text" name="phone" placeholder="Insira seu telefone">
     <br>
     <input type="hidden" name="status" value="0">
