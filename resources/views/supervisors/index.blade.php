22511221b47030ffde4d7f16d19db66815e01440
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2025-11-24 08:42:52 -0300

    :sparkles: feat: adiciona mensagem de sucesso para cadastro e edição de supervisores

--- resources/views/supervisors/index.blade.php
+++ resources/views/supervisors/index.blade.php
@@ -1,4 +1,7 @@
 <h1>Listagem de supervisores</h1>
+@if( session('success') )
+    <p>{{ session('success') }}</p>
+@endif
 <a href="{{ route('supervisors.create') }}">+ Adicionar</a>
 @foreach ($supervisors as $supervisor)
     <p> {{ $supervisor->name}} | <a href="{{ route('supervisors.edit', ['supervisor' => $supervisor->id]) }}">✏</a> | <form action="{{ route('supervisors.destroy', ['supervisor' => $supervisor->id]) }}" method="post">
