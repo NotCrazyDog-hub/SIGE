16057b0a32ae0c9577d8308173a4a50e879306bd
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-28 12:17:06 -0300

    :sparkles: feat: adiciona mensagem de sucesso para cadastro e edição de departamentos

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -1,4 +1,7 @@
 <h1>Listagem de departamentos</h1>
+@if( session('success') )
+    <p>{{ session('success') }}</p>
+@endif
 <a href="{{ route('departments.create') }}">+ Adicionar</a>
 @foreach ($departments as $department)
     <p>{{ $department->name }} | <a href="{{ route('departments.edit', ['department' => $department->id]) }}">✏</a> | <form action="{{ route('departments.destroy', ['department' => $department->id]) }}" method="post">
