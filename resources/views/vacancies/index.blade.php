11e1097bceea30d57ef0b98bf5911b583e9b0950
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-28 11:30:07 -0300

    :sparkles: feat: adiciona mensagem de sucesso para cadastro e edição de vagas

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -1,4 +1,7 @@
 <h1>Listagem de vagas</h1>
+@if( session('success') )
+    <p>{{ session('success') }}</p>
+@endif
 <a href="{{ route('vacancies.create') }}">+ Adicionar</a>
 @foreach ($vacancies as $vacancy)
     <p>{{ $vacancy->title }} | <a href="{{ route('vacancies.edit', ['vacancy' => $vacancy->id]) }}">✏</a> | <form action="{{ route('vacancies.destroy', ['vacancy' => $vacancy->id]) }}" method="post">
