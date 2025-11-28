bccd5aa10d947bd8d008bdd00794294a337fa300
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-11-28 11:42:58 -0300

    :sparkles: feat: adiciona mensagem de erro para edição de estagiários

--- resources/views/interns/edit.blade.php
+++ resources/views/interns/edit.blade.php
@@ -1,4 +1,7 @@
 <h1>Edição de estagiários</h1>
+@if( session('error') )
+    <p>{{ session('error') }}</p>
+@endif
 <form action="{{ route('interns.update', ['intern' => $intern->id]) }}" method="post">
     @csrf
     @method('PUT')
